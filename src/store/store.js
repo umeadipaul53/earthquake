0c04bdbc9d60e629d0a66ed0ee417c7e846a72b7
umeadipaul53 <umeadipaul53@gmail.com>
2025-09-18 13:10:29 +0100

    updated redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
-import earthquakeReducer  from "../reducers/earthquakeReducer";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import earthquakeReducer from "../reducers/earthquakeReducer";
+
+//persist config
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["data"],
+};
+
+const persistedEarthquakeReducer = persistReducer(
+  persistConfig,
+  earthquakeReducer
+);
 
 const store = configureStore({
   reducer: {
-    earthquake: earthquakeReducer,
+    earthquake: persistedEarthquakeReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // needed for redux-persist
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
